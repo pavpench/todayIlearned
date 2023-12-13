459d0ac6bcf47d308231a1cba9de43d76249371d
pavpench <p.penchev90@gmail.com>
2023-12-13 12:33:33 +0100

    minor share a fact button refactor

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ console.log("Hello world");
 const shareButton = document.querySelector(".btn-large");
 const form = document.querySelector(".fact-form");
 
+const toggleForm = function () {
+  form.classList.contains("hidden")
+    ? form.classList.remove("hidden")
+    : form.classList.add("hidden");
+};
+
 shareButton.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
-    form.classList.remove("hidden");
     shareButton.textContent = "Close";
   } else {
-    form.classList.add("hidden");
     shareButton.textContent = "Share a Fact";
   }
+  toggleForm();
 });
