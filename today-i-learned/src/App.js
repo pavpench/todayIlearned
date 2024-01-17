9fc94bb6b405e89bb316f580cf26b657b1261a03
pavpench <p.penchev90@gmail.com>
2024-01-17 22:39:04 +0100

    vote buttons, and minor styling additions

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -13,54 +13,6 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
-// function Counter() {
-//   let [count, setCount] = useState(0);
-//   return (
-//     <div>
-//       <span style={{ fontSize: "40px" }}>{count}</span>
-//       <button
-//         className="btn btn-large"
-//         onClick={() => setCount((current) => current + 1)}>
-//         +1
-//       </button>
-//     </div>
-//   );
-// }
-
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
@@ -284,10 +236,15 @@ function FactList({ facts, setFacts }) {
 }
 
 function Fact({ fact, setFacts }) {
-  async function handleVote() {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
+
+  async function handleVote(columnName) {
+    setIsUpdating(true);
     const { data: updatedFact, error } = await supabase
       .from("facts")
-      .update({ votesInteresting: fact.votesInteresting + 1 })
+      .update({ [columnName]: fact[columnName] + 1 })
       .eq("id", fact.id)
       .select();
 
@@ -295,12 +252,14 @@ function Fact({ fact, setFacts }) {
       setFacts((facts) =>
         facts.map((f) => (f.id === fact.id ? updatedFact[0] : f))
       );
+    setIsUpdating(false);
   }
 
   return (
     <>
       <li className="fact">
         <p>
+          {isDisputed ? <span className="disputed">[DISPUTED]</span> : null}
           {fact.text}
           <a href={fact.source} className="source" target="_blank">
             (Source)
@@ -316,15 +275,17 @@ function Fact({ fact, setFacts }) {
           {fact.category}
         </span>
         <div className="vote-buttons">
-          <button onClick={handleVote}>
+          <button
+            onClick={() => handleVote("votesInteresting")}
+            disabled={isUpdating}>
             👍
             <strong>{fact.votesInteresting}</strong>
           </button>
-          <button onClick={handleVote}>
+          <button onClick={() => handleVote("votesMindblowing")}>
             🤯
             <strong>{fact.votesMindblowing}</strong>
           </button>
-          <button onClick={handleVote}>
+          <button onClick={() => handleVote("votesFalse")}>
             ⛔️
             <strong>{fact.votesFalse}</strong>
           </button>
